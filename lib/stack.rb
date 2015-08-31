281626aabf309188067f36fbc0d6b5d3337ab9d1
apradhn <abhijit.pradhan6@gmail.com>
2015-08-31 15:23:34 -0400

    writes logic for stack solution

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,17 @@
 class Stack
+  def initialize
+    @stack = []
+  end
 
+  def push(element)
+    @stack << element
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def size
+    @stack.size
+  end
 end
