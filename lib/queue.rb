00823506595eb79eea7c93ba1c8cc2ecace8d05c
apradhn <abhijit.pradhan6@gmail.com>
2015-08-31 15:25:56 -0400

    writes logic for soultion to queue spec

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,15 @@
 require_relative 'stack'
 
 class Queue
+  def initialize
+    @queue = []
+  end
 
+  def enqueue(element)
+    @queue.push(element)
+  end
+
+  def dequeue
+    @queue.shift
+  end
 end
